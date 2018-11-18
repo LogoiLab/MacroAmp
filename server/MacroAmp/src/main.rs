a2f6f6c6be6f21bc28ae415a8b9be374b42d6689
Chad Baxter <cbaxter@mail.umw.edu>
2018-11-18 05:01:13 -0500

    added functions

--- server/MacroAmp/src/main.rs
+++ server/MacroAmp/src/main.rs
@@ -16,6 +16,27 @@ use self::db::*;
 use self::schema::*;
 
 use self::session::Session;
+use rocket::data::Data;
+use rocket::Outcome;
+use rocket::http::Status;
+use rocket::request::{self, Request, FromRequest};
+
+use std::io;
+use std::path::{Path, PathBuf};
+use std::fs::remove_file;
+
+use rocket::response::NamedFile;
+
+#[allow(proc_macro_derive_resolution_fallback)]
+
+struct ID(String);
+
+#[derive(Debug)]
+enum IDError {
+    BadCount,
+    Missing,
+    Invalid,
+}
 
 #[post("/", format = "application/json", data = "<session>")]
 fn create(session: Json<Session>, connection: db::Connection) -> Json<Session> {
@@ -29,11 +50,19 @@ fn read_all(connection: db::Connection) -> Json<Value> {
 }
 
 #[get("/<id>")]
+fn is_paused(id: i32, connection: db::Connection) -> Json<Value> {
+    let sessions: Vec<Session> = Session::is_paused(&connection, id);
+    let pause: String = format!("{}", sessions[0].paused);
+    Json(json!(pause))
+}
+
+
+#[get("/<id>")]
 fn read(id: i32, connection: db::Connection) -> Json<Value> {
     Json(json!(Session::read(&connection, id)))
 }
 
-#[put("/<id>", format = "application/json", data = "<session>")]
+#[post("/<id>", format = "application/json", data = "<session>")]
 fn update(id: i32, session: Json<Session>, connection: db::Connection) -> Json<Value> {
     let update = Session { id: Some(id), ..session.into_inner() };
     Json(json!({
@@ -43,21 +72,47 @@ fn update(id: i32, session: Json<Session>, connection: db::Connection) -> Json<V
 
 #[delete("/<id>")]
 fn delete(id: i32, connection: db::Connection) -> Json<Value> {
+    remove_file(format!("./static/{}.mp3", &id)).unwrap();
     Json(json!({
         "success": Session::delete(id, &connection)
     }))
 }
 
-#[post("/upload", format = "plain", data = "<data>")]
-fn upload(data: Data) -> io::Result<String> {
-    data.stream_to_file("/tmp/upload.mp3").map(|n| n.to_string())
+#[post("/upload", format = "text/plain", data = "<data>")]
+fn upload(data: Data, sess_id: ID) -> std::io::Result<String> {
+    let path: String = format!("./static/{}.mp3", sess_id.0);
+    data.stream_to_file(path).map(|n| n.to_string())
 }
 
+fn is_valid(key: &str) -> bool {
+    key.len() <= 6
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for ID {
+    type Error = IDError;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let keys: Vec<_> = request.headers().get("sess_id").collect();
+        match keys.len() {
+            0 => Outcome::Failure((Status::BadRequest, IDError::Missing)),
+            1 if is_valid(keys[0]) => Outcome::Success(ID(keys[0].to_string())),
+            1 => Outcome::Failure((Status::BadRequest, IDError::Invalid)),
+            _ => Outcome::Failure((Status::BadRequest, IDError::BadCount)),
+        }
+    }
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("./static/").join(file)).ok()
+}
 
 fn main() {
     rocket::ignite()
         .manage(db::connect())
-        .mount("/session", routes![create, update, delete, read])
+        .mount("/session", routes![create, update, delete, read, upload])
         .mount("/sessions", routes![read_all])
+        .mount("/static", routes![files])
+        .mount("/status", routes![is_paused])
         .launch();
 }
