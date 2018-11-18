b7e900c0b2652662c215e7cc5f46b5c7dd1fe444
Chad Baxter <cbaxter@mail.umw.edu>
2018-11-17 23:21:20 -0500

    lol

--- server/MacroAmp/src/main.rs
+++ server/MacroAmp/src/main.rs
@@ -4,87 +4,86 @@
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 extern crate rand;
-
 #[cfg(test)] mod tests;
 
 use std::sync::Mutex;
 use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-use rand::{thread_rng, Rng};
+//use rand::{thread_rng, Rng};
 use rocket::State;
 use rocket_contrib::json::{Json, JsonValue};
+
 
 // The type to represent the ID of a message.
 type ID = usize;
 
 // We're going to store all of the messages here. No need for a DB.
-type ReqMap = Mutex<HashMap<ID, String>>;
 type SessionMap = Mutex<HashMap<ID, Session>>;
 type Epoch = u64;
-
-#[derive(Serialize, Deserialize)]
-struct Req {
-    id: Option<ID>,
-    contents: String
-}
 
 #[derive(Serialize, Deserialize, Clone)]
 struct Session {
     id: ID,
     start_at: Option<u64>,
+    data: Option<String>
 
 }
 
 // TODO: This example can be improved by using `route` with multiple HTTP verbs.
-#[post("/<id>", format = "json", data = "<req>")]
-fn new(id: ID, req: Json<Req>, map: State<ReqMap>) -> JsonValue {
-    let mut hashmap = map.lock().expect("map lock.");
-    if hashmap.contains_key(&id) {
-        json!({
-            "status": "error",
-            "reason": "ID exists. Try put."
-        })
-    } else {
-        hashmap.insert(id, req.0.contents);
-        json!({ "status": "ok" })
-    }
-}
 
 #[put("/<id>", format = "json", data = "<req>")]
-fn update(id: ID, req: Json<Req>, map: State<ReqMap>) -> Option<JsonValue> {
+fn update(id: ID, req: Json<Session>, map: State<SessionMap>) -> Option<JsonValue> {
     let mut hashmap = map.lock().unwrap();
-    if hashmap.contains_key(&id) {
-        hashmap.insert(id, req.0.contents);
-        Some(json!({ "status": "ok" }))
+    if hashmap.contains_key(&id.clone()) {
+        hashmap.insert(id.clone(), req.0);
+        let mut updated: Session = Session{id: 0, start_at: None, data: None};
+        hashmap.get(&id.clone()).clone().map(|sess| {
+            let old_sess = sess.clone();
+            updated = Session{id: old_sess.id, start_at: Some(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 5), data: old_sess.data};
+        });
+        hashmap.insert(id.clone(), updated);
+        Some(json!({
+            "code": "200",
+            "status": "ok"
+        }))
     } else {
         None
     }
 }
 
 #[get("/<id>", format = "json")]
-fn join(id: ID, map: State<ReqMap>) -> Option<Json<Req>> {
-    let hashmap = map.lock().unwrap();
-    hashmap.get(&id).map(|contents| {
-        Json(Req {
-            id: Some(id),
-            contents: contents.clone()
+fn join(id: ID, map: State<SessionMap>) -> Option<Json<Session>> {
+    let mut hashmap = map.lock().unwrap();
+    let mut updated: Session = Session{id: 0, start_at: None, data: None};
+    hashmap.get(&id.clone()).clone().map(|sess| {
+        let old_sess = sess.clone();
+        updated = Session{id: old_sess.id, start_at: Some(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 5), data: old_sess.data};
+    });
+    hashmap.insert(id.clone(), updated);
+    hashmap.get(&id.clone()).map(|session| {
+        Json(Session {
+            id: session.id,
+            start_at: session.start_at,
+            data: session.data
         })
     })
 }
 
-#[post("/new_session")]
-fn new_session(map: State<SessionMap>) -> JsonValue {
+#[post("/<id>", format = "json", data = "<req>")]
+fn new_session(id: ID, req: Json<Session>, map: State<SessionMap>) -> JsonValue {
     let mut hashmap = map.lock().unwrap();
-    let sess: Session = gen_session();
-    hashmap.insert(sess.id, sess.clone());
-    json!({"id": sess.id})
-}
-
-fn gen_session() -> Session {
-    let mut rng = thread_rng();
-    let id: usize = rng.gen_range(0, 999999);
-    Session{id: id, start_at: None}
+    hashmap.insert(id.clone(), req.0);
+    let mut updated: Session = Session{id: 0, start_at: None, data: None};
+    hashmap.get(&id.clone()).clone().map(|sess| {
+        let old_sess = sess.clone();
+        updated = Session{id: old_sess.id, start_at: Some(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 5), data: old_sess.data};
+    });
+    hashmap.insert(id.clone(), updated);
+    json!({
+        "code": "200",
+        "status": "ok"
+    })
 }
 
 #[catch(404)]
@@ -107,9 +106,9 @@ fn server_error() -> JsonValue {
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/req", routes![new, update, join, new_session])
+        .mount("/req", routes![update, join, new_session])
         .register(catchers![not_found, server_error])
-        .manage(Mutex::new(HashMap::<ID, String>::new()))
+        .manage(Mutex::new(HashMap::<ID, Session>::new()))
 }
 
 fn main() {
